1506f42393605201df3cc55b743572b38e6c40a5
廖翊翔 <82229244+yixiang-liao@users.noreply.github.com>
2025-06-17 11:39:53 +0800

    After report

--- fontend/src/layouts/Footer.jsx
+++ fontend/src/layouts/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
       <p className="footer-title">相信音樂 大數據分析平台</p>
       <p className="footer-course">113-2 大數據分析與實務 期末報告</p>
       <p className="footer-course">資管三乙 C111118207 廖翊翔</p>
-      <p className="footer-copy">© 2025 I-Hsiang, Lial . All rights reserved.</p>
+      <p className="footer-copy">© 2025 I-Hsiang, Liao . All rights reserved.</p>
     </footer>
   )
 }
